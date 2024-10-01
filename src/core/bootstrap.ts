2cbc327228c9106f38d6dca9ca0b6c933c2977ce
adil-solulab <adil@solulab.co>
2024-10-01 08:14:24 +0530

    initial commit
    Signed-off-by: adil-solulab <adil@solulab.co>

--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -11,6 +11,33 @@ export default async (app: Application) => {
 			CreateServerStateDefaults(),
 			JWTHelper.GenerateKeys(), // #2 Generate Public and Private Keys if don't exist
 		])
+		console.log({
+			PORT: process.env.PORT,
+			ENVIRONMENT: process.env['NODE_ENV'],
+			DB_CONNECTION_STRING: process.env.DEV_DB_CONNECTION_STRING,
+			DB_CONNECTION_OPTIONS: {
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			},
+
+			ITEMS_PER_PAGE: parseInt(process.env.ITEMS_PER_PAGE),
+			SALT_ROUNDS: parseInt(process.env.SALT_ROUNDS),
+			JWT_SECRET: process.env.JWT_SECRET,
+			JWT_EXPIRY: process.env.JWT_EXPIRY,
+
+			SUPPORT_EMAIL: process.env.SUPPORT_EMAIL,
+
+			NODE_MAILER: {
+				HOST: process.env.NODE_MAILER_HOST,
+				EMAIL: process.env.NODE_MAILER_EMAIL,
+				PASS: process.env.NODE_MAILER_PASS,
+				PORT: parseInt(process.env.NODE_MAILER_PORT),
+				SENDER: process.env.NODE_MAILER_SENDER,
+				RECEIVER: process.env.NODE_MAILER_RECEIVER,
+			},
+			THRESHOLD_PERCENTAGE: parseInt(process.env.THRESHOLD_PERCENTAGE),
+			SECONDARY_DB: process.env.SECONDARY_DB,
+		})
 	} catch (error) {
 		Logger.error(error)
 	}
